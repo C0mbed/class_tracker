7dd3b62d0edb89bafeea9ad2a929930b9b4c900c
Lantz Warrick <betalantz@gmail.com>
2020-09-16 18:22:55 -0700

    adds prof seeds to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+profs = {"L. Warrick": "CS", "M. Farncombe": "JD", "C Joaquin": "CJ"}
+
+profs.each {|n, d| Professor.create(name: n, department: d, tenure: true)}
